6304ff4d875e9a3ce201afb9444e8c3e03c2a6a0
Đỗ Thanh Bình <binhdothanh.navn@gmail.com>
2022-07-21 20:29:22 +0700

    add agency

---
+++ src/AutoLike.Domain.Shared/Agencies/CommonStatus.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoLike.Agencies
+{
+    public enum CommonStatus
+    {
+        Active, Pause, Pending, Delete
+    }
+}
---
+++ src/AutoLike.Domain/Agencies/Agency.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities.Auditing;
+
+namespace AutoLike.Agencies
+{
+    public class Agency : FullAuditedAggregateRoot<Guid>
+    {
+		public Guid UserId { get; set; }
+		public string AccountName { get; set; }
+		public string BankName { get; set; }
+		public string BankNumber { get; set; }
+		public string Fullname { get; set; } 
+		public string Domain { get; set; } 
+		public decimal Balance { get; set; } 
+		public string Title { get; set; } 
+		public string Address { get; set; } 
+		public string Contact { get; set; }  
+		public CommonStatus Status { get; set; }  
+		public string AppSecretKey { get; set; }  
+		public string[] WhiteListIP { get; set; } 
+    }
+}
