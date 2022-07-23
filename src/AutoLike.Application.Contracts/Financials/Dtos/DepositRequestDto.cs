ba38560f5f58b7e0e6fd6629db6e5ebee2643ed6
Đỗ Thanh Bình <binhdothanh.navn@gmail.com>
2022-07-23 22:28:04 +0700

    update deposit

--- src/AutoLike.Application.Contracts/Financials/Dtos/DepositRequestDto.cs
+++ src/AutoLike.Application.Contracts/Financials/Dtos/DepositRequestDto.cs
@@ -3,12 +3,12 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Text;
+using System.Xml.Linq;
 
 namespace AutoLike.Financials.Dtos
 {
     public class DepositRequestDto
-    { 
-        public decimal Amount { get; set; }
-        public FinancialUnit Unit { get; set; }
+    {
+        public decimal Amount { get; set; } 
     }
 }
