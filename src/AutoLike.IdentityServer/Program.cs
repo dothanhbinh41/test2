1461db6114420099853d09eead00e22dad60f7f2
Đỗ Thanh Bình <binhdothanh.navn@gmail.com>
2022-07-27 20:02:10 +0700

    update port

--- src/AutoLike.IdentityServer/Program.cs
+++ src/AutoLike.IdentityServer/Program.cs
@@ -33,7 +33,7 @@ public class Program
             builder.Host.AddAppSettingsSecretsJson()
                 .UseAutofac()
                 .UseSerilog();
-            builder.WebHost.UseUrls("http://localhost:44315");
+            builder.WebHost.UseUrls("http://0.0.0.0:6000");
             await builder.AddApplicationAsync<AutoLikeIdentityServerModule>();
             var app = builder.Build(); 
             await app.InitializeApplicationAsync();
